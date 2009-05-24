2caed9b2a038776b12f880160b2c06d732be893b
Matheus E. Muller <hello@memuller.com>
2009-05-24 18:29:08 -0300

    Added specs. They're all quite messy now, and on a single file, but they're something, at least.

--- spec/gcal_fetcher_spec.rb
+++ spec/gcal_fetcher_spec.rb
@@ -1,7 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
-describe "GcalFetcher" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+begin
+@sinatra_pid = start_sinatra
+describe GcalFetcher::Connection do
+  before(:each)do
+    @c = GcalFetcher::Connection.new
   end
+  
+  it "should load the test feed" do
+    @c.entries.should_not be_empty
+  end
+  it "should return an empty array upon failure" do
+    @c.fetch('localhost:4567/null.xml')
+    @c.entries.should == []
+  end
+  it "should have all entries sanitized" do
+    @c.entries.first.content << "<script>evil tag of doom!</script>"
+    @c.sanitize!
+    @c.entries.first.content['<script>'].should be nil
+  end
+
+end
+
+describe "feed information" do
+  before :all do
+  
+  end
+  it "should have a title"
+
+  it "should have an author"
+
+  it "should have a begin date"
+
+  it "should have an status"
+
+end
+
+describe "grouped feeds" do
+  before :all do
+  
+  end
+  
+  it "should have all events mapped"
+
+  it "all events should have dates"
+
+end
+ensure
+stop_sinatra(@sinatra_pid)
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,22 @@
 require 'spec'
-
+require 'rubygems'
+require 'redgreen'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'gcal_fetcher'
+require 'lib/gcal_fetcher'
 
-Spec::Runner.configure do |config|
-  
+Spec::Runner.configure do |config| 
+
+end
+
+def start_sinatra
+  pid = Process.fork do
+    exec "ruby #{File.join(File.dirname(__FILE__), '..','servers', 'server.rb')}"
+  end
+  sleep 0.5
+  pid
+end
+
+def stop_sinatra(pid)
+  Process.kill 'HUP', pid
 end
