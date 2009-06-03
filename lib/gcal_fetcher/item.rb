726becf169dca14a0b877c5bb643be1ca472b2e2
Matheus E. Muller <hello@memuller.com>
2009-06-03 08:18:08 -0300

    Fixed bug when parsing date (splitting in the ':' broke the time strings). But there's still a bug there.

--- lib/gcal_fetcher/item.rb
+++ lib/gcal_fetcher/item.rb
@@ -1,28 +1,35 @@
 module GcalFetcher
   class Item
-    attr_accessor :title, :author, :begins_at, :ends_at, :fields, :status
+    attr_accessor :title, :author, :begins_at, :ends_at, :fields, :status, :duration
     def initialize(entry)
-      field_names = {:pt => %w(Quando Quem Situação), :en => ['When','Who','Event Status'], :sys => %w(begins_at author status)}
-      #puts entry.title
-      @title = entry.title
+      field_names = {:pt => %w(Quando Quem Situação Descrição), :en => ['When','Who','Event Status', 'Description'], :sys => %w(begins_at author status)}
+      time_range_separators = {:pt => ' a ', :en => ' to '}
       br = entry.content.include?('<br />') ? '<br />' : '<br>'
       field_names.keys.each do |language|
         fields = field_names[language] and break if field_names[language].select{|name| entry.content[name]}.size > 0
+        time_range_separator = time_range_separators[:language]
       end
-        i = 0
-        entry.content.split(br).each do |line|
-          line.strip!
-          unless line.empty?
-             var_name = line.split(':').first and var_value = line.split(':').last
-             if i == 0 #Begin date
-               @begins_at = @ends_at = DateTime.parse(var_value)
-             else
-              instance_variable_set("@"+field_names[:sys][i], var_value)
-             end
-             i += 1
+      @title = entry.title 
+      i = 0
+      (lines = entry.content.split(br)).each do |line|
+        line.strip!
+        unless line.empty?
+          var_name = line.split(':').first and var_value = line.gsub "#{var_name}:", ""
+          if i == 0 #first one's the date.
+            if var_value.include? time_range_separator
+              @begins_at = DateTime.parse(var_value.split(time_range_separator).first) and @ends_at = DateTime.parse(var_value.split(time_range_separator).last)
+              @duration = @ends_at - @begins_at
+            else
+              @begins_at = @ends_at = DateTime.parse(var_value)
+            end
+          elsif i == lines.size - 1 #last one, can be the description.
+          
+          else
+            instance_variable_set("@"+field_names[:sys][i], var_value)
           end
+          i = i.next
         end
-
+      end
     end
   end
 end
